7ad8e95cba556e8d9596a7a68e45393b89e7cf91
Peter Wasiewicz <pwasiewi@elka.pw.edu.pl>
2018-04-10 01:23:25 +0200

    new task agenda

---
+++ task_agenda/task_agenda.ino
@@ -0,0 +1,53 @@
+/** 
+ *  TaskScheduler Test
+ *  https://github.com/gioblu/Agenda
+
+  Pinouts:
+                                      +-----+
+         +----[PWR]-------------------| USB |--+
+         |                            +-----+  |
+         |         GND/RST2  [ ][ ]            |
+         |       MOSI2/SCK2  [ ][ ]  A5/SCL[ ] |   C5
+         |          5V/MISO2 [ ][ ]  A4/SDA[ ] |   C4
+         |                             AREF[ ] |
+         |                              GND[ ] |
+         | [ ]N/C                    SCK/13[ ] |  PB5
+         | [ ]IOREF                 MISO/12[ ] |   .
+         | [ ]RST                   MOSI/11[ ]~|   .
+         | [ ]3V3    +---+               10[ ]~|   .
+         | [ ]5v    -| A |-               9[ ]~|   .
+         | [ ]GND   -| R |-               8[ ] |  PB0
+         | [ ]GND   -| D |-                    |
+         | [ ]Vin   -| U |-               7[ ] |  PD7
+         |          -| I |-               6[ ]~|   .
+     PC0 | [ ]A0    -| N |-               5[ ]~|   .
+      .  | [ ]A1    -| O |-               4[ ] |   .
+      .  | [ ]A2     +---+           INT1/3[ ]~|   .
+      .  | [ ]A3                     INT0/2[ ] |   .
+      .  | [ ]A4/SDA  RST SCK MISO     TX>1[ ] |   .
+     PC5 | [ ]A5/SCL  [ ] [ ] [ ]      RX<0[ ] |  PD0
+         |            [ ] [ ] [ ]              |
+         |  UNO_R3    GND MOSI 5V  ____________/
+          \_______________________/
+
+      http://busyducks.com/ascii-art-arduinos
+
+*/
+#include <Agenda.h>
+
+Agenda scheduler;
+
+void setup() {
+  pinMode(13, OUTPUT);
+  scheduler.insert(blink, 1000000);
+}
+
+void blink() {
+  digitalWrite(13, HIGH);
+  delay(10);
+  digitalWrite(13, LOW);
+}
+
+void loop() {
+  scheduler.update();
+};
