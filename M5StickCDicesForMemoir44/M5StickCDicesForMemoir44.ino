74b680cb845f61b537654ee59529b0937e7ba9c7
Piotr Wąsiewicz <pwasiewi@mail.com>
2020-01-23 20:13:20 +0100

    Dices for Memoir in M5Stick

--- M5StickCDicesForMemoir44/M5StickCDicesForMemoir44.ino
+++ M5StickCDicesForMemoir44/M5StickCDicesForMemoir44.ino
@@ -2,6 +2,8 @@
   Up to 8 dices on a tiny 80x160px TFT display
   Author: Piotr Wasiewicz <pwasiewi(at)gmail.com>
   based on two dices Alfonso de Cala demo version
+  and some ideas from M5StickC_Nixie_tube_Clock
+  and FactoryTest Basic M5StickC Example
 */
 
 #include "Timers.h"                 // Timers from Polish forum arduino http://bit.ly/arduinotimer
@@ -11,16 +13,6 @@ Timers <2> akcja;
 #include "DiceMemoir.c"
 
 const uint8_t*n[] = { Soldier, Grenade, Star, Flag, Tank, Soldier };
-
-#define DOT_SIZE 6
-int dot[6][6][2] {
-  {{35, 35}},
-  {{15, 15}, {55, 55}},
-  {{15, 15}, {35, 35}, {55, 55}},
-  {{15, 15}, {15, 55}, {55, 15}, {55, 55}},
-  {{15, 15}, {15, 55}, {35, 35}, {55, 15}, {55, 55}},
-  {{15, 15}, {15, 35}, {15, 55}, {55, 15}, {55, 35}, {55, 55}},
-};
 
 float accX = 0;
 float accY = 0;
@@ -41,6 +33,11 @@ void setup(void) {
   M5.Lcd.setTextSize(3);
   M5.Lcd.print("SHAKE ME");
   M5.Axp.ScreenBreath(led_count);
+  M5.IMU.getAccelData(&accX, &accY, &accZ);
+  randint = (int)1000 * (accX + accY + accZ);
+  randomSeed(micros() + randint % 100);
+  pinMode(M5_LED, OUTPUT);
+  digitalWrite(M5_LED, HIGH);
   pinMode(M5_BUTTON_HOME, INPUT);
   pinMode(M5_BUTTON_RST, INPUT);
   akcja.attach(0, 100, sensors); // Thread 1
@@ -65,10 +62,6 @@ void sensors() {
     }
   } else {
     M5.IMU.getAccelData(&accX, &accY, &accZ);
-    //randint = (int)1000 * (accX + accY + accZ);
-    // Serial.println("Seed=");
-    // Serial.println(micros()+randint%100);
-    //randomSeed(micros() + randint % 100);
     if (accX > 1.5 ||  accY > 1.5 ) {
       depict();
     }
@@ -97,36 +90,30 @@ void buttons() {
 
 void depict() {
   M5.update();
+  digitalWrite(M5_LED, LOW);
+  M5.Lcd.fillScreen(TFT_RED);
+  delay(1500);  // A little delay to increase suspense :-D
   M5.Lcd.fillScreen(TFT_GREEN);
+  digitalWrite(M5_LED, HIGH);
   if (dice_count == 1) {
     // Draw first dice
-    delay(500);  // A little delay to increase suspense :-D
     int number = random(0, 6);
-    //draw_dice(45, 5, number, 70);
-    M5.Lcd.pushImage(45, 5, 36, 36, (uint16_t *)n[number]);
+    M5.Lcd.pushImage(62, 22, 36, 36, (uint16_t *)n[number]);
   } else if (dice_count == 2) {
     // Draw first dice
-    delay(500);  // A little delay to increase suspense :-D
     int number = random(0, 6);
-    //draw_dice(5, 5, number, 70);
-    M5.Lcd.pushImage(5, 5, 36, 36, (uint16_t *)n[number]);
+    M5.Lcd.pushImage(22, 22, 36, 36, (uint16_t *)n[number]);
     // Draw second dice
-    //delay(50);
     number = random(0, 6);
-    //draw_dice(85, 5, number, 70);
-    M5.Lcd.pushImage(85, 5, 36, 36, (uint16_t *)n[number]);
+    M5.Lcd.pushImage(102, 22, 36, 36, (uint16_t *)n[number]);
   } else {
-    delay(500);  // A little delay to increase suspense :-D
     for (int i = 1; i <= dice_count; i++) {
       int number = random(0, 6);
       if (dice_count < 5) {
-        //draw_dice(2 + (i - 1) * 40, 22, number, 36);
         M5.Lcd.pushImage(2 + (i - 1) * 40, 22, 36, 36, (uint16_t *)n[number]);
       } else if (i < 5)
-        //draw_dice(2 + (i - 1) * 40, 2, number, 36);
         M5.Lcd.pushImage(2 + (i - 1) * 40, 2, 36, 36, (uint16_t *)n[number]);
       else
-        //draw_dice(2 + (i - 5) * 40, 42, number, 36);
         M5.Lcd.pushImage(2 + (i - 5) * 40, 42, 36, 36, (uint16_t *)n[number]);
     }
   }
@@ -134,15 +121,4 @@ void depict() {
 
 void loop() {
   akcja.process();
-}
-
-void draw_dice(int16_t x, int16_t y, int n, int edge) {
-  int divider = 1;
-  if (edge < 70) divider = 2;
-  M5.Lcd.fillRect(x, y, edge, edge, WHITE);
-  for (int d = 0; d < 6; d++) {
-    if (dot[n][d][0] > 0) {
-      M5.Lcd.fillCircle(x + dot[n][d][0] / divider, y + dot[n][d][1] / divider, DOT_SIZE, TFT_BLACK);
-    }
-  }
 }
