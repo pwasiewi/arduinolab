ef7bb8894a62066d370193de768c1f6f0ed34fa6
Piotr Wąsiewicz <pwasiewi@mail.com>
2020-01-23 20:27:09 +0100

    Light level and time limit set in Dices on M5StickC

--- M5StickCDices/M5StickCDices.ino
+++ M5StickCDices/M5StickCDices.ino
@@ -2,6 +2,7 @@
   Up to 8 dices on a tiny 80x160px TFT display
   Author: Piotr Wasiewicz <pwasiewi(at)gmail.com>
   based on two dices Alfonso de Cala demo version
+  and FactoryTest Basic M5StickC Example
 */
 
 #include "Timers.h"                 // Timers from Polish forum arduino http://bit.ly/arduinotimer
@@ -24,7 +25,7 @@ float accY = 0;
 float accZ = 0;
 int randint = 1;
 int dice_count = 1;
-int led_count = 7;
+int led_count = 8;
 int sleep_count = 0;
 
 void setup(void) {
@@ -37,6 +38,12 @@ void setup(void) {
   M5.Lcd.setCursor(10, 30);
   M5.Lcd.setTextSize(3);
   M5.Lcd.print("SHAKE ME");
+  M5.Axp.ScreenBreath(led_count);
+  M5.IMU.getAccelData(&accX, &accY, &accZ);
+  randint = (int)1000 * (accX + accY + accZ);
+  randomSeed(micros() + randint % 100);
+  pinMode(M5_LED, OUTPUT);
+  digitalWrite(M5_LED, HIGH);
   pinMode(M5_BUTTON_HOME, INPUT);
   pinMode(M5_BUTTON_RST, INPUT);
   akcja.attach(0, 100, sensors); // Thread 1
@@ -61,10 +68,6 @@ void sensors() {
     }
   } else {
     M5.IMU.getAccelData(&accX, &accY, &accZ);
-    randint = (int)1000 * (accX + accY + accZ);
-    // Serial.println("Seed=");
-    // Serial.println(micros()+randint%100);
-    randomSeed(micros() + randint % 100);
     if (accX > 1.5 ||  accY > 1.5 ) {
       depict();
     }
@@ -81,6 +84,9 @@ void buttons() {
     M5.Axp.ScreenBreath(led_count); //screen light control
   }
   if (digitalRead(M5_BUTTON_HOME) == LOW) {
+    // M5.IMU.getAccelData(&accX, &accY, &accZ);
+    randint = (int) 1000 * (accX + accY + accZ);
+    randomSeed(micros() + randint % 100);
     dice_count++;
     if (dice_count > 8)
       dice_count = 1;
@@ -90,23 +96,23 @@ void buttons() {
 
 void depict() {
   M5.update();
+  digitalWrite(M5_LED, LOW);
+  M5.Lcd.fillScreen(TFT_RED);
+  delay(1000);  // A little delay to increase suspense :-D
   M5.Lcd.fillScreen(TFT_GREEN);
+  digitalWrite(M5_LED, HIGH);
   if (dice_count == 1) {
     // Draw first dice
-    delay(500);  // A little delay to increase suspense :-D
     int number = random(0, 6);
     draw_dice(45, 5, number, 70);
   } else if (dice_count == 2) {
     // Draw first dice
-    delay(500);  // A little delay to increase suspense :-D
     int number = random(0, 6);
     draw_dice(5, 5, number, 70);
     // Draw second dice
-    //delay(50);
     number = random(0, 6);
     draw_dice(85, 5, number, 70);
   } else {
-    delay(500);  // A little delay to increase suspense :-D
     for (int i = 1; i <= dice_count; i++) {
       int number = random(0, 6);
       if (dice_count < 5) {
